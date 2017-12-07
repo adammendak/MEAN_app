ed0acfaab141595563c844d10cc87060abb90cd9
adammendak <adam.mendak@gmail.com>
2017-12-07 08:37:30 +0100

    connecting back to front

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,15 +1,39 @@
 import { Message } from "./message.model";
+import {Http, Response, Headers} from "@angular/http";
+import {Injectable} from "@angular/core";
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
 
+@Injectable()
 export class MessageService {
     private messages: Message[] = [];
 
+    constructor(private http: Http) {}
+
     addMessage(message: Message) {
         this.messages.push(message);
-        console.log(this.messages);
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type:' : 'application/json'});
+        console.log(body);
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+            .map((response: Response) => {
+                response.json();
+            })
+            .catch((error: Response) => Observable.throw('error in message service'));
     }
 
     getMessages() {
-        return this.messages;
+        return this.http.get('http://localhost:3000/message')
+            .map((response: Response) => {
+            const messages = response.json().obj;
+            let transformedMessages: Message[] = [];
+            for(let message of messages) {
+                transformedMessages.push(new Message(message.content, 'Dummy', message.id, null));
+            }
+            this.messages = transformedMessages;
+            return transformedMessages;
+            })
+            .catch((error: Response) => Observable.throw('error in message service'));
     }
 
     deleteMessage(message: Message) {
---
+++ routes/messages.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+var Message = require('../models/message');
+
+router.get('/', function (req, res, next) {
+   Message.find()
+       .exec(function (err, docs) {
+            if(err) {
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'success',
+                obj: docs
+            })
+       });
+});
+
+
+router.post('/', function (req, res, next) {
+    var message = new Message({
+        content: req.body.content
+    });
+    message.save(function (err, result) {
+        if(err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: 'saved message',
+            obj: result
+        })
+    })
+
+    res.render('index');
+});
+
+module.exports = router;
